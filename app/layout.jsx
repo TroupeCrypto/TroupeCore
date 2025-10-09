f7a866ededd15979183a2446649d5d2835cbb2a0
TroupeCrypto <thegungareport@gmail.com>
2025-10-08 20:34:55 -0400

    Initial Next.js minimal web tree

---
+++ app/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = { title: "Troupe Inc", description: "Welcome" };
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
