0516f6afcd26fc6302056ced17e2029765bee22c
TroupeCrypto <thegungareport@gmail.com>
2025-10-12 13:12:52 -0400

    Fix import path in route.js

--- app/api/vault/news/route.js
+++ app/api/vault/news/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { newsItems } from "lib/data/vault";
+import { newsItems } from "../../../../lib/data/vault.js";
 
 export function GET() {
   return NextResponse.json({ data: newsItems });
